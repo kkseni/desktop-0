bcdc2f6a190bef69c0c8e543238d7c123894d476
Oksana-Dyka15888 <kseniadika2210@gmail.com>
2022-08-29 15:15:53 +0300

    new

---
+++ src/js/modal/modalClose.js
@@ -0,0 +1,45 @@
+import { backdrop } from "./markupModal";
+import { controlScreen } from "./controlScreen"; 
+
+export class ModalClose {
+
+    body = document.querySelector('body');
+
+    onToggle = () => {
+
+      const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');   
+
+        backdrop.classList.toggle('visibleV');
+        if (!document.querySelector('.visibleV')) {
+            this.blockScroll('auto', '', '', '');
+            return window.scrollTo(0, parseInt(this.body.style.top || '0') * -1);
+        }
+
+        else { return  this.blockScroll('scroll', `${scrollY}`, `${controlScreen()}`, 'fixed'); }
+       };
+
+   
+    blockScroll = (ov, top, wd, p) => {
+       document.querySelector('body').style.overflowY = `${ov}`;
+        document.querySelector('body').style.position = `${p}`;
+         document.querySelector('body').style.width = `${wd}`;
+      return  document.querySelector('body').style.top = `-${top}`;
+     };
+    
+
+     funcKeyDown = (e) => {
+         if (e.code === 'Escape') {
+            this.onToggle();
+           return !backdrop.classList.contains('visibleV') && window.removeEventListener('keydown', this.funcKeyDown);   
+         }
+         return;        
+    };
+
+     funcClickBackdrop = e => {
+        if (e.target === e.currentTarget) {
+            this.onToggle();
+         }
+    };
+}
+
+
